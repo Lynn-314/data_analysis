3a88315943ab1031777bf6853e95c9defb230318
Lynn-314 <72400896+Lynn-314@users.noreply.github.com>
2021-11-20 01:26:49 +0530

    Update cricket_explore.sql

--- cricket_explore.sql
+++ cricket_explore.sql
@@ -1,3 +1,24 @@
 CREATE DATABASE cric;
 USE cric;
 SELECT * FROM `cricket-info-with-venue-country`;
+
+### total number of matches played
+SELECT count(ID) as `number of matches` FROM `cricket-info-with-venue-country`;
+
+### total number of matches played for each type
+SELECT `Type`, count(ID)  as `number of matches` FROM `cricket-info-with-venue-country`
+GROUP BY `Type`;
+
+### performance statistics
+SELECT `Result`, count(ID)  as `number of matches` FROM `cricket-info-with-venue-country`
+GROUP BY `Result`;
+
+### performance statistics outside India and in India
+(SELECT `Result`, count(ID)  as `number of matches` FROM `cricket-info-with-venue-country`
+WHERE venue_country != "India"
+GROUP BY `Result`)
+UNION
+(SELECT `Result`, count(ID)  as `number of matches` FROM `cricket-info-with-venue-country`
+WHERE venue_country = "India"
+GROUP BY `Result`)
+;
